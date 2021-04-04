2a728dca3e6bc6f7d31a57cbb04fe442c0dfdb95
6john <80368724+6john@users.noreply.github.com>
2021-04-03 21:22:05 -0400

    Add files via upload

---
+++ Week2/index.js
@@ -0,0 +1,43 @@
+//1
+
+var isHotOutside = true;
+
+var isWeekday = false;
+
+var hasMoneyInPocket = false;
+
+//2
+
+ var costOfMilk = 9;
+
+ var moneyInWallet = 18;
+
+ var thirstLevel = 9;
+ 
+//3
+
+var shouldByIcecream = true;
+
+if(isHotOutside == true && moneyInWallet > 0){
+    console.log('Will Buy Icecream');
+}
+
+var willGoSwimming = true;
+if(isHotOutside == true && isWeekday == false){
+    console.log('Will Swim')
+}
+
+var isAGoodDay = true;
+if(isHotOutside == true && isWeekday == false && moneyInWallet > 0){
+    console.log('Today Is a Good Day!')
+}
+
+var willBuyMilk = true;
+if(isHotOutside == true && moneyInWallet >= 18 && thirstLevel >= 3){
+    console.log('Will Buy Milk')
+}
+
+
+
+//4
+
---
+++ Week2/loops.js
@@ -0,0 +1,32 @@
+// 1
+console.log('Loop 1')
+
+var i = 0; 
+while(i <= 98){ 
+   i += 2; 
+   console.log(); 
+} 
+
+// 2
+console.log('Loop 2')
+
+var i = 100; 
+while(i >= 3){ 
+   i -= 3;
+   console.log(); 
+} 
+
+//3
+console.log('Loop 3')
+
+for(let i = 0; i < 100; i++){
+    i += 1
+    console.log()
+}
+
+//4
+console.log('Loop 4')
+
+for(let i = 0; i < 100; i++){
+    console.log(i)
+}
